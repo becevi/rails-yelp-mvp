7250a53c402ecf65959d4899538ff40cc976367c
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-08-12 15:45:02 -0500

    Added validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
-class Restaurant < ApplicationRecord
+class Restaurant < ApplicationRecord 
+  # CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
+  
+  validates :name, :address, :category, presence: true
+                  
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
 end
